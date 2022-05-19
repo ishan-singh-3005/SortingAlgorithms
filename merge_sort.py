14c02ff947b7d375f188789b9b7daf4391d94952
ishan-singh-3005 <ishan423@gmail.com>
2022-05-18 20:31:58 -0400

    Created merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,43 @@
+def mergesort(lst: list) -> list:
+    """Return a sorted list with the same elements as <lst>.
+
+    This is a *non-mutating* version of mergesort; it does not mutate the
+    input list.
+    """
+    if len(lst) < 2:
+        return lst[:]
+    else:
+        # Divide the list into two parts, and sort them recursively.
+        mid = len(lst) // 2
+        left_sorted = mergesort(lst[:mid])
+        right_sorted = mergesort(lst[mid:])
+
+        # Merge the two sorted halves.
+        return _merge(left_sorted, right_sorted)
+
+
+def _merge(lst1: list, lst2: list) -> list:
+    """Return a sorted list with the elements in <lst1> and <lst2>.
+
+    Precondition: <lst1> and <lst2> are sorted.
+    """
+    index1 = 0
+    index2 = 0
+    merged = []
+
+    while index1 < len(lst1) and index2 < len(lst2):
+        if lst1[index1] <= lst2[index2]:
+            merged.append(lst1[index1])
+            index1 += 1
+        else:
+            merged.append(lst2[index2])
+            index2 += 1
+
+    # Now either index1 == len(lst1) or index2 == len(lst2).
+    assert index1 == len(lst1) or index2 == len(lst2)
+    # The remaining elements of the other list
+    # can all be added to the end of <merged>.
+    # Note that at most ONE of lst1[index1:] and lst2[index2:]
+    # is non-empty, but to keep the code simple, we include both.
+    return merged + lst1[index1:] + lst2[index2:]
+    
